9aaef5c147e15df2a99a054aafc66a569f5bb5d9
jerem-y445 <jau@njit.edu>
2025-06-08 22:30:42 -0400

    Added a simple keyboard shortcut to test push button

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#include "USB.h"
+#include "USBHIDKeyboard.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define BUTTON1 1
+#define BUTTON2 2
+#define BUTTON3 3
+#define BUTTON4 4
+
+USBHIDKeyboard Keyboard;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  USB.begin();
+  Keyboard.begin();
+  delay(2000);
+  Keyboard.println("Hello from Arduino USB HID!");
+
+  pinMode(BUTTON1, INPUT_PULLDOWN);
+  pinMode(BUTTON2, INPUT_PULLDOWN);
+  pinMode(BUTTON3, INPUT_PULLDOWN);
+  pinMode(BUTTON4, INPUT_PULLDOWN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (digitalRead(BUTTON1)) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('t');
+    delay(100);
+    Keyboard.releaseAll();
+    delay(500); // for debounce delay to prevent multiple presses from being detectedHello from Arduino USB HID!
+    
+  }
 }
